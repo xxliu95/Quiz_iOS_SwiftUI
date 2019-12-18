ec39ad461c356d5954c3c44aa9752d9427d0dddc
Xinxin Liu <xxliu95@gmail.com>
2019-12-18 16:07:30 +0100

    Initial Commit

---
+++ P5_Quiz10/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P5_Quiz10
+//
+//  Created by Xinxin Liu on 18/12/2019.
+//  Copyright © 2019 Xinxin Liu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
